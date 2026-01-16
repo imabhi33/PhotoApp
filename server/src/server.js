de661b2232269fb2e305ae41d66d882b5131ec02
Abhijit Mishra <imabhijit333@gmail.com>
2026-01-17 04:32:10 +0530

    change in server

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || '*',
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
